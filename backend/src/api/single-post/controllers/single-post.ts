d814ff72d262256c8122ab8b9f68b05572e644f0
shogowada1999 <81617513+shogowada1999@users.noreply.github.com>
2024-07-20 19:31:58 +0900

    update: strapi v5 rc

--- backend/src/api/single-post/controllers/single-post.ts
+++
@@ -1,7 +0,0 @@
-/**
- * single-post controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::single-post.single-post');
--- backend/src/api/single-post/routes/single-post.ts
+++
@@ -1,7 +0,0 @@
-/**
- * single-post router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::single-post.single-post');
