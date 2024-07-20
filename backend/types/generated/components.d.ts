7439f52e8faea81edcda043c7f376532c483e7dc
shogowada1999 <81617513+shogowada1999@users.noreply.github.com>
2024-07-20 22:49:06 +0900

    add: component text link

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,29 @@
 import type { Struct, Schema } from '@strapi/strapi';
 
+export interface LinkTextLink extends Struct.ComponentSchema {
+  collectionName: 'components_link_text_links';
+  info: {
+    displayName: 'Text Link';
+    icon: 'link';
+  };
+  attributes: {
+    text: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        minLength: 1;
+      }>;
+    url: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        minLength: 1;
+      }>;
+  };
+}
+
 declare module '@strapi/strapi' {
-  export module Public {}
+  export module Public {
+    export interface ComponentSchemas {
+      'link.text-link': LinkTextLink;
+    }
+  }
 }
