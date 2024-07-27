6524642e4298b255455a80011d5d8839090ed39d
shogowada1999 <81617513+shogowada1999@users.noreply.github.com>
2024-07-27 23:01:25 +0900

    add: nuxt font packages and setting

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,8 +1,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
-  colorMode: {
-    classSuffix: '',
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxt/fonts'],
+  colorMode: { classSuffix: '' },
+  fonts: {
+    families: [
+      { name: 'Barlow-ExtraBold', src: '/Barlow-ExtraBold.ttf' },
+      { name: 'NotoSansJP-ExtraBold', src: '/NotoSansJP-ExtraBold.ttf' },
+      { name: 'NotoSansJP-Medium', src: '/NotoSansJP-Medium.ttf' },
+    ],
   },
 });
