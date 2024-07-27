8509692c8dfc450fec2d6343928edd00421e431d
shogowada1999 <81617513+shogowada1999@users.noreply.github.com>
2024-07-27 16:24:51 +0900

    add: tailwindcss packages and setting

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  darkMode: 'class',
+  content: [
+    './app.vue',
+    './components/**/*.{js,ts,vue}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
