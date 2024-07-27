612ad63f52c8b0e7eaf9c5eac74cb30bc0518b14
shogowada1999 <81617513+shogowada1999@users.noreply.github.com>
2024-07-27 16:45:17 +0900

    add: nuxt-color-mode packages and setting

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,8 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss"]
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+  colorMode: {
+    classSuffix: '',
+  },
 });
